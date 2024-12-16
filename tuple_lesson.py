59ab4c8e4585a27dec702ec47b4cbec6d2e27555
smithbwlhs <smithb@wlwv.k12.or.us>
2024-12-16 10:00:47 -0800

    updated

--- tuple_lesson.py
+++ tuple_lesson.py
@@ -13,22 +13,29 @@ On Sunday I don't talk about them. :)
 # The look like an ordered pair in math
 
 # Make tuple with two items
-
+my_tuple = ("3",4)
+print(my_tuple)
 # Make tuple with one item
-
+lonely_tuple = (5,) # you have to have both ( ) and the ,
+print(lonely_tuple)
 
 # You access elements from them using [ ] just like strings
-
-
+print(my_tuple[0])
 
 # Also like strings, you can only concatenate tuples with other tuples
+not_lonely_tuple = my_tuple + (5,)
+print(not_lonely_tuple)
+
+integer_tuple = (int(my_tuple[0]),4,5)
+print(integer_tuple)
 
 # I heard you like tuples...
 # so you can also put tuples inside your tuples
 # need two [] to access them.
+character_info = (("Rosalie",100),("Cedric",75),("Shana",94))
+print(character_info[1][0]) # return "Cedric"
 
-
-########## Exercise ##########
+########## HW ##########
 
 """
 In the file linear_calculations.py, complete the slope and distance
